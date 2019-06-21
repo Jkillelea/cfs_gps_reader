a4c0efdb638bb67248ae32a80169ea24777472b6
Jacob Killelea <jkillelea@protonmail.ch>
2019-06-21 00:01:01 -0700

    software bus messages for the nmea messages

---
+++ fsw/platform_inc/gps_reader_msgs.h
@@ -0,0 +1,39 @@
+#ifndef _GPS_READER_MSGS_H_
+#define _GPS_READER_MSGS_H_
+
+#include "cfe.h"
+#include "nmea/nmea.h"
+
+typedef struct {
+    uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+    nmeaINFO gpsInfo;
+} GpsInfoMsg_t;
+
+typedef struct {
+    uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+    nmeaGPGGA gpsGpgga;
+} GpsGpggaMsg_t;
+
+typedef struct {
+    uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+    nmeaGPGSA gpsGpgsa;
+} GpsGpgsaMsg_t;
+
+// TODO: not linking against libmath properly, nmea_info2GPGSV fails
+// typedef struct {
+//     uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+//     nmeaGPGSV gpsGpgsv;
+// } GpsGpgsvMsg_t;
+
+typedef struct {
+    uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+    nmeaGPRMC gpsGprmc;
+} GpsGprmcMsg_t;
+
+typedef struct {
+    uint8 tlmHeader[CFE_SB_TLM_HDR_SIZE];
+    nmeaGPVTG gpsGpvtg;
+} GpsGpvtgMsg_t;
+
+
+#endif // _GPS_READER_MSGS_H_
