64e3381f599da1c3b90211ce416350e9f58c525e
Jacob Killelea <Jkillelea@protonmail.ch>
2019-06-25 22:09:44 -0700

    rename err_logmsg to error_logmsg

--- fsw/src/gps_reader_events.h
+++ fsw/src/gps_reader_events.h
@@ -12,7 +12,6 @@
 #ifndef _GPS_READER_EVENTS_H_
 #define _GPS_READER_EVENTS_H_
 
-
 #define GPS_READER_RESERVED_EID              0
 #define GPS_READER_STARTUP_INF_EID           1 
 #define GPS_READER_COMMANDRST_INF_EID        4
@@ -20,7 +19,7 @@
 #define GPS_READER_LEN_ERR_EID               6 
 #define GPS_READER_LOOP_LOGMSG               7
 #define GPS_READER_INFO_LOGMSG               8
-#define GPS_READER_ERR_LOGMSG                9
+#define GPS_READER_ERROR_LOGMSG              9
 
 #endif /* _GPS_READER_EVENTS_H_ */
 
